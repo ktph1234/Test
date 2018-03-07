d1a5aa727db0225d8f38018a9e1b91ce242e713f
Praveen Jose <me@example.com>
2018-03-07 17:31:37 +0800

    Gommitting

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-//some comments
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
